e7a1e4069326d730a6adbaae0fa3bc60660e189e
Anamika Yadav <aanyayadav419@gmail.com>
2023-04-06 00:53:35 +0530

    all set

---
+++ server/index.js
@@ -0,0 +1,29 @@
+
+const express = require('express')
+const cors = require('cors');
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+// app.use(cors());
+
+const dbConfig = require("./config/dbConfig");
+
+
+const usersRoute = require("./routes/usersRoute");
+const examsRoute = require("./routes/examsRoute");
+const resportsRoute = require("./routes/reportsRoute");
+
+app.use("/api/users", usersRoute);
+app.use("/api/exams", examsRoute);
+app.use("/api/reports", resportsRoute);
+
+const port = process.env.PORT || 8080;
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
